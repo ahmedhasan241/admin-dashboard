3a8c6178ee524f46f52a4ef0da4550182247b26c
ahmedhasan241 <92892508+ahmedhasan241@users.noreply.github.com>
2023-10-04 17:33:36 +0300

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
       div
         label.form-label(for="email") Email address
         div.my-2
-          Field(id="email" name="email" type="email" placeholder="EXP : test@gmail.com" v-bind="emailValue" autocomplete="email" class="form-input")
+          Field(id="email" name="email" type="email" placeholder="EXP : test@gmail.com" v-bind="emailValue" autocomplete="email" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500")
         ErrorMessage(name="email" class="my-2")
       div
         div.flex.items-center.justify-between
           label.form-label(for="password") Password
         div.my-2
-          Field(id="password" name="password" type="password" placeholder="EXP : 123456test" autocomplete="current-password" class="form-input")
+          Field(id="password" name="password" type="password" placeholder="EXP : 123456test" autocomplete="current-password" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500")
         ErrorMessage(name="password" class="my-2")
         span.error(v-if="loginError") {{ loginError }}
       div
